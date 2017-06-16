2f8fcf566d6fcd26c6be17289d4b37b9beabc675
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-06-16 15:48:11 +0200

    commit

---
+++ pdb2xyz_lib/include/Keyword.h
@@ -0,0 +1,17 @@
+#include <string>
+
+#ifndef PDB2XYZ_KEYWORD
+#define PDB2XYZ_KEYWORD
+
+namespace PCA{
+class Keyword{
+public:
+    static std::string header;
+    static std::string atom;
+    static std::string end;
+    static std::string endOfModel;
+    
+
+};
+}// end of namespace;
+#endif
---
+++ pdb2xyz_lib/source/Keywords.cpp
@@ -0,0 +1,10 @@
+#include "Keyword.h"
+
+using namespace PCA;
+using namespace std;
+
+string Keyword::header = "HEADER";
+string Keyword::atom = "ATOM";
+string Keyword::end = "END";
+string Keyword::endOfModel = "ENDMDL";
+
