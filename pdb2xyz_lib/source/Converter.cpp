b5e771dd627fa7110ed516f559a0d2113a3f9d2c
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-06-20 17:37:51 +0200

    works

--- pdb2xyz_lib/source/Converter.cpp
+++ pdb2xyz_lib/source/Converter.cpp
@@ -332,10 +332,8 @@ bool Converter::printTheLongestPart(std::ofstream& fout, char chain, int model){
     int haveMissings = true; 
     int havingRepeats = false;
     
-    if(model==0)
-	tmp_model = 0;
-    else
-	tmp_model = model;
+
+    tmp_model = model-1;
 
     resNum = get<5>(data[tmp_model][chainDelimiter[chainNum]]);
     partDelimiter.push_back(0);
@@ -391,69 +389,34 @@ cout<<resNum<<" Missing before\n"<<flush;
     }
     
     // print the longest part without repeated atoms
-    
-    if(model>0){
-	fout<<atomsInPart[maxPart]<<"\n";
-	fout<<proteinName;
-	if(numModels>1)
-	    fout<<"\tMODEL "<<model;
-	fout<<"\t"<<chain;
-	if(haveMissings){
-	    fout<<"\tpart from atom "<<(get<5>(data[model-1][partDelimiter[maxPart]]));
-	    fout<<" to "<<(get<5>(data[model-1][partDelimiter[maxPart+1]]));
-	}
-	fout<<"\n";
-	int countRepeat = 0;
-	for(int i=partDelimiter[maxPart]; i<partDelimiter[maxPart+1]; i++){
+    fout<<atomsInPart[maxPart]<<"\n";
+    fout<<proteinName;
+    if(numModels>1)
+	fout<<"\tMODEL "<<model;
+    fout<<"\t"<<chain;
+    if(haveMissings){
+	fout<<"\tpart from atom "<<(get<5>(data[model-1][partDelimiter[maxPart]]));
+	fout<<" to "<<(get<5>(data[model-1][partDelimiter[maxPart+1]]));
+    }
+    fout<<"\n";
+    int countRepeat = 0;
+    for(int i=partDelimiter[maxPart]; i<partDelimiter[maxPart+1]; i++){
 
-	    //don't print repeated atoms
-	    if(havingRepeats){
-		if(repeatedAtom[countRepeat]==i){
-		    countRepeat++;
-		    continue;
-		}
+	//don't print repeated atoms
+	if(havingRepeats){
+	    if(repeatedAtom[countRepeat]==i){
+		countRepeat++;
+		continue;
 	    }
-	    //------
+	}
+	//------
 	
-	    fout<<get<3>(data[model-1][i])<<"\t"; // print element symbol
-	    fout<<get<0>(data[model-1][i])<<"\t"; // print x
-	    fout<<get<1>(data[model-1][i])<<"\t"; // print y
-	    fout<<get<2>(data[model-1][i])<<"\n"; // print z
-	}
+	fout<<get<3>(data[model-1][i])<<"\t"; // print element symbol
+	fout<<get<0>(data[model-1][i])<<"\t"; // print x
+	fout<<get<1>(data[model-1][i])<<"\t"; // print y
+	fout<<get<2>(data[model-1][i])<<"\n"; // print z
     }
-    
-    else{
-	for(int k=0; k<numModels; k++){
-	    fout<<atomsInPart[maxPart]<<"\n";
-	    fout<<proteinName;
-	    if(numModels>1)
-		fout<<"\tMODEL "<<k+1;
-	    fout<<"\t"<<chain;
-	    if(haveMissings){
-		fout<<"\tpart from atom "<<(get<5>(data[k][partDelimiter[maxPart]]));
-		fout<<" to "<<(get<5>(data[k][partDelimiter[maxPart+1]]));
-	    }
-	    fout<<"\n";
-	    
-	    int countRepeat = 0;
-	    for(int i=partDelimiter[maxPart]; i<partDelimiter[maxPart+1]; i++){
-	    
-		//don't print repeated atoms
-		if(havingRepeats){
-		    if(repeatedAtom[countRepeat]==i){
-			countRepeat++;
-			continue;
-		    }
-		}
-		//------
-		fout<<get<3>(data[k][i])<<"\t"; // print element symbol
-		fout<<get<0>(data[k][i])<<"\t"; // print x
-		fout<<get<1>(data[k][i])<<"\t"; // print y
-		fout<<get<2>(data[k][i])<<"\n"; // print z
-	    }
-	}
-    }
-    
+
 return haveMissings;
 }
 
